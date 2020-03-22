94100a8aba73babc9ad7d29135bd5ec0c5c0df28
Vitali Serada <gomelska@gmail.com>
2020-03-22 18:44:07 +0300

    feat: add human readable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function toReadable (number) {
-  
-}
+  let toTwenty = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+  'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+  'seventeen', 'eighteen', 'nineteen'];
+  let toHundred = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty',
+  'ninety'];
+  let big = ['hundred', 'thousand', 'million'];
+
+  if (number === 0 ) {
+    return 'zero';
+  };
+
+  let StringedNumber = number.toString();
+  let numberLength = StringedNumber.length;
+
+  if (number < 20) {
+    return toTwenty[number];
+  };
+  if (numberLength === 2) {
+    return (toHundred[StringedNumber[0]] + ' ' + toTwenty[StringedNumber[1]]).trim();      
+  };
+  let result = '';
+  if (numberLength === 3) {
+    if (StringedNumber[1] === '0' && StringedNumber[2] === '0') {
+      return toTwenty[StringedNumber[0]] + ' hundred';
+    } else if (StringedNumber[1] === '0') {
+      return toTwenty[StringedNumber[0]] + ' hundred ' + toTwenty[StringedNumber[2]];
+    } else {
+      let toCheck = parseInt(StringedNumber.slice(1, 3));
+      if (toCheck < 20) {
+        return toTwenty[StringedNumber[0]] + ' hundred ' + toTwenty[toCheck];
+      } else {
+        let toCheckStr = StringedNumber.slice(1, 3);
+        return (toTwenty[StringedNumber[0]] + ' hundred ' + toHundred[toCheckStr[0]] + ' ' + toTwenty[toCheckStr[1]]).trim();
+      };
+    };
+  };
+};
